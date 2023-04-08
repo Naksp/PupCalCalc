7dab817b45ad08dd403c4dea8d75c30a84256f98
Naksp <iw12635@gmail.com>
2023-04-08 17:02:51 -0500

    add stuff to learn page

--- src/dog-group.tsx
+++ src/dog-group.tsx
@@ -11,278 +11,278 @@ const KG = 'kg';
 const LBS = 'lbs';
 
 const Multipliers = {
-  PUPPY_0_TO_4_MONTS: 3.0,
-  PUPPY_4_TO_12_MONTHS: 2.0,
+	PUPPY_0_TO_4_MONTS: 3.0,
+	PUPPY_4_TO_12_MONTHS: 2.0,
 
-  INTACT_ADULT: 1.8,
-  NEUTERED_ADULT: 1.6,
+	INTACT_ADULT: 1.8,
+	NEUTERED_ADULT: 1.6,
 
-  INACTIVE_MIN: 1.2,
-  INACTIVE_MAX: 1.4,
-  ACTIVE_MIN: 2.0,
-  ACTIVE_MAX: 5.0,
+	INACTIVE_MIN: 1.2,
+	INACTIVE_MAX: 1.4,
+	ACTIVE_MIN: 2.0,
+	ACTIVE_MAX: 5.0,
 
-  WEIGHT_LOSS: 1.0,
-  WEIGHT_GAIN_MIN: 1.2,
-  WEIGHT_GAIN_MAX: 1.8,
+	WEIGHT_LOSS: 1.0,
+	WEIGHT_GAIN_MIN: 1.2,
+	WEIGHT_GAIN_MAX: 1.8,
 }
 
 // Radios
 const ageRadios = [
-  { name: '<4 months', value: PUPPY_0_TO_4_MONTS },
-  { name: '4-12 months', value: PUPPY_4_TO_12_MONTHS },
-  { name: '>1 year', value: ADULT }
+	{ name: '<4 months', value: PUPPY_0_TO_4_MONTS },
+	{ name: '4-12 months', value: PUPPY_4_TO_12_MONTHS },
+	{ name: '>1 year', value: ADULT }
 ];
 
 const activityRadios = [
-  { name: 'Inactive', value: 'inactive' },
-  { name: 'Average', value: 'moderatelyActive' },
-  { name: 'Active', value: 'active' },
+	{ name: 'Inactive', value: 'inactive' },
+	{ name: 'Average', value: 'moderatelyActive' },
+	{ name: 'Active', value: 'active' },
 ];
 
 const neuteredRadios = [
-  { name: 'Neutered', value: 'neutered' },
-  { name: 'Intact', value: 'intact' },
+	{ name: 'Neutered', value: 'neutered' },
+	{ name: 'Intact', value: 'intact' },
 ];
 
 
 function DogGroup(props: {
-  setSubmitEnabled: (enabled: boolean) => void,
-  onBalancedCaloriesChange: (calories: CaloriePair) => void,
-  onBalancedCaloriesResultChange: (result: string) => void,
-  includeTreats: boolean,
-  onTreatsCaloriesResultChange: (result: string) => void,
+	setSubmitEnabled: (enabled: boolean) => void,
+	onBalancedCaloriesChange: (calories: CaloriePair) => void,
+	onBalancedCaloriesResultChange: (result: string) => void,
+	includeTreats: boolean,
+	onTreatsCaloriesResultChange: (result: string) => void,
 }) {
 
-  const [weightInput, setWeightInput] = useState<string>('');
-  const [weightUnit, setWeightUnit] = useState<string>(LBS);
+	const [weightInput, setWeightInput] = useState<string>('');
+	const [weightUnit, setWeightUnit] = useState<string>(LBS);
 
-  const [balancedCalories, setBalancedCalories] = useState<CaloriePair>({min: -1, max: -1});
-  const [treatsCalories, setTreatsCalories] = useState<CaloriePair>({min: -1, max: -1});
+	const [balancedCalories, setBalancedCalories] = useState<CaloriePair>({ min: -1, max: -1 });
+	const [treatsCalories, setTreatsCalories] = useState<CaloriePair>({ min: -1, max: -1 });
 
-  const [multiplier, setMultiplier] = useState<number>(3.0);
-  const [isAdult, setIsAdult] = useState<boolean>(false);
-  const [isNeutered, setIsNeutered] = useState<boolean>(true);
+	const [multiplier, setMultiplier] = useState<number>(3.0);
+	const [isAdult, setIsAdult] = useState<boolean>(false);
+	const [isNeutered, setIsNeutered] = useState<boolean>(true);
 
-  const [ageRadioValue, setAgeRadioValue] = useState<string>(ageRadios[0].value);
-  const [activityRadioValue, setActivityRadioValue] = useState<string>('');
-  const [neuteredRadioValue, setNeuteredRadioValue] = useState<string>('');
+	const [ageRadioValue, setAgeRadioValue] = useState<string>(ageRadios[0].value);
+	const [activityRadioValue, setActivityRadioValue] = useState<string>('');
+	const [neuteredRadioValue, setNeuteredRadioValue] = useState<string>('');
 
-  useEffect(() => {
+	useEffect(() => {
 
-    var min: number;
-    var max: number;
+		var min: number;
+		var max: number;
 
-    // Display calorie range if activity level is 'active'
-    if (isAdult && activityRadioValue === 'active') {
-      min = Math.round(calculateCalories(Multipliers.ACTIVE_MIN));
-      max = Math.round(calculateCalories(Multipliers.ACTIVE_MAX));
+		// Display calorie range if activity level is 'active'
+		if (isAdult && activityRadioValue === 'active') {
+			min = Math.round(calculateCalories(Multipliers.ACTIVE_MIN));
+			max = Math.round(calculateCalories(Multipliers.ACTIVE_MAX));
 
-    } else if (isAdult && activityRadioValue === 'inactive') {
-      min = Math.round(calculateCalories(Multipliers.INACTIVE_MIN));
-      max = Math.round(calculateCalories(Multipliers.INACTIVE_MAX));
+		} else if (isAdult && activityRadioValue === 'inactive') {
+			min = Math.round(calculateCalories(Multipliers.INACTIVE_MIN));
+			max = Math.round(calculateCalories(Multipliers.INACTIVE_MAX));
 
-    } else {
-      min = Math.round(calculateCalories(multiplier));
-      max = -1
+		} else {
+			min = Math.round(calculateCalories(multiplier));
+			max = -1
 
-    }
+		}
 
-    if (props.includeTreats) {
-      setBalancedCalories({min: Math.round(min * 0.9), max: Math.round(max * 0.9)});
-      setTreatsCalories({min: min * 0.1, max: max * 0.1});
-    } else {
-      setBalancedCalories({min: min, max: max});
-    }
+		if (props.includeTreats) {
+			setBalancedCalories({ min: Math.round(min * 0.9), max: Math.round(max * 0.9) });
+			setTreatsCalories({ min: min * 0.1, max: max * 0.1 });
+		} else {
+			setBalancedCalories({ min: min, max: max });
+		}
 
-  }, [activityRadioValue, isAdult, multiplier, weightInput, props.includeTreats]);
+	}, [activityRadioValue, isAdult, multiplier, weightInput, props.includeTreats]);
 
-  const calculateCalories = (multiplier: number): number => {
-    const rer: number = caclulateRer(Number(weightInput));
+	const calculateCalories = (multiplier: number): number => {
+		const rer: number = caclulateRer(Number(weightInput));
 
-    return rer * multiplier;
-  }
+		return rer * multiplier;
+	}
 
-  const caclulateRer = (weight: number): number => {
-    if (weightUnit === LBS) {
-      weight = weight / 2.205;
-    }
-    return 70 * Math.pow(weight, 0.75);
-  }
+	const caclulateRer = (weight: number): number => {
+		if (weightUnit === LBS) {
+			weight = weight / 2.205;
+		}
+		return 70 * Math.pow(weight, 0.75);
+	}
 
-  useEffect(() => {
-    props.onBalancedCaloriesChange(balancedCalories);
-  }, [balancedCalories])
+	useEffect(() => {
+		props.onBalancedCaloriesChange(balancedCalories);
+	}, [balancedCalories])
 
 
-  useEffect(() => {
+	useEffect(() => {
 
-    var {min, max} = balancedCalories;
+		var { min, max } = balancedCalories;
 
-    // if (props.includeTreats) {
-    //   min *= Math.round(0.9);
-    //   max *= Math.round(0.9);
-    // }
+		// if (props.includeTreats) {
+		//   min *= Math.round(0.9);
+		//   max *= Math.round(0.9);
+		// }
 
-    if (max > 0) {
-      props.onBalancedCaloriesResultChange(`${min.toString()} - ${max.toString()} food calories/day`);
-      props.onTreatsCaloriesResultChange(`${(Math.round(treatsCalories.min)).toString()} - ${(Math.round(treatsCalories.max)).toString()} treats calories/day`);
-    } else {
-      props.onBalancedCaloriesResultChange(`${min.toString()} food calories/day`);
-      props.onTreatsCaloriesResultChange(`${(Math.round(treatsCalories.min)).toString()} treat calories/day`);
-    }
+		if (max > 0) {
+			props.onBalancedCaloriesResultChange(`${min.toString()} - ${max.toString()} food calories/day`);
+			props.onTreatsCaloriesResultChange(`${(Math.round(treatsCalories.min)).toString()} - ${(Math.round(treatsCalories.max)).toString()} treats calories/day`);
+		} else {
+			props.onBalancedCaloriesResultChange(`${min.toString()} food calories/day`);
+			props.onTreatsCaloriesResultChange(`${(Math.round(treatsCalories.min)).toString()} treat calories/day`);
+		}
 
-  }, [balancedCalories, treatsCalories, props.includeTreats])
+	}, [balancedCalories, treatsCalories, props.includeTreats])
 
-  const handleAgeChange = (value: string) => {
-    if (value === ADULT) {
-      setIsAdult(true);
+	const handleAgeChange = (value: string) => {
+		if (value === ADULT) {
+			setIsAdult(true);
 
-    } else {
-      setIsAdult(false);
+		} else {
+			setIsAdult(false);
 
-      if (value === PUPPY_0_TO_4_MONTS) {
-        setMultiplier(Multipliers.PUPPY_0_TO_4_MONTS);
-      } else {
-        setMultiplier(Multipliers.PUPPY_4_TO_12_MONTHS);
-      }
-    }
+			if (value === PUPPY_0_TO_4_MONTS) {
+				setMultiplier(Multipliers.PUPPY_0_TO_4_MONTS);
+			} else {
+				setMultiplier(Multipliers.PUPPY_4_TO_12_MONTHS);
+			}
+		}
 
-    setAgeRadioValue(value);
-  }
+		setAgeRadioValue(value);
+	}
 
-  useEffect(() => {
-    if (isAdult) {
-      handleActivityChange(activityRadioValue);
-    }
-    //FIXME I'm guessing this is a code smell
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isAdult])
+	useEffect(() => {
+		if (isAdult) {
+			handleActivityChange(activityRadioValue);
+		}
+		//FIXME I'm guessing this is a code smell
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isAdult])
 
-  const handleActivityChange = (value: string) => {
-    setActivityRadioValue(value);
-    if (value === activityRadios[0].value) {
-      setMultiplier(Multipliers.INACTIVE_MIN);
-    } else if (value === activityRadios[1].value) {
-      setMultiplier(isNeutered ? Multipliers.NEUTERED_ADULT : Multipliers.INTACT_ADULT);
-    } else {
-      setMultiplier(Multipliers.ACTIVE_MAX);
-    }
-  }
+	const handleActivityChange = (value: string) => {
+		setActivityRadioValue(value);
+		if (value === activityRadios[0].value) {
+			setMultiplier(Multipliers.INACTIVE_MIN);
+		} else if (value === activityRadios[1].value) {
+			setMultiplier(isNeutered ? Multipliers.NEUTERED_ADULT : Multipliers.INTACT_ADULT);
+		} else {
+			setMultiplier(Multipliers.ACTIVE_MAX);
+		}
+	}
 
-  const handleNeuteredChange = (value: string) => {
-    setNeuteredRadioValue(value);
-    setIsNeutered(value === 'neutered');
-  }
+	const handleNeuteredChange = (value: string) => {
+		setNeuteredRadioValue(value);
+		setIsNeutered(value === 'neutered');
+	}
 
-  useEffect(() => {
-    if (activityRadioValue === activityRadios[1].value) {
-      setMultiplier(isNeutered ? Multipliers.NEUTERED_ADULT : Multipliers.INTACT_ADULT);
-    }
-  }, [activityRadioValue, isNeutered]);
+	useEffect(() => {
+		if (activityRadioValue === activityRadios[1].value) {
+			setMultiplier(isNeutered ? Multipliers.NEUTERED_ADULT : Multipliers.INTACT_ADULT);
+		}
+	}, [activityRadioValue, isNeutered]);
 
-  const handleWeightInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const val = e.currentTarget.value;
-    props.setSubmitEnabled(!!val);
-    setWeightInput(val);
-  }
+	const handleWeightInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const val = e.currentTarget.value;
+		props.setSubmitEnabled(!!val);
+		setWeightInput(val);
+	}
 
-  useEffect(() => {
-    if (ageRadioValue === ADULT) {
-      props.setSubmitEnabled(activityRadioValue !== '' && neuteredRadioValue !== '' && !!weightInput);
-    } else if (weightInput) {
-      props.setSubmitEnabled(true);
-    }
+	useEffect(() => {
+		if (ageRadioValue === ADULT) {
+			props.setSubmitEnabled(activityRadioValue !== '' && neuteredRadioValue !== '' && !!weightInput);
+		} else if (weightInput) {
+			props.setSubmitEnabled(true);
+		}
 
-  }, [activityRadioValue, ageRadioValue, neuteredRadioValue]);
+	}, [activityRadioValue, ageRadioValue, neuteredRadioValue]);
 
-  return (
-    <div>
-      <h1 className='mt-2'>dog</h1>
-      <Row className='mb-4 mt-2 justify-content-center'>
-        <Col className='col-6 col-sm-4'>
-          <InputGroup>
-            <Form.Control id='weightInput' type='number' step='any' placeholder='##' onChange={handleWeightInputChange} />
-            <DropdownButton id='weightDropdown' title={weightUnit}>
-              <DropdownItem onClick={() => setWeightUnit(LBS)}>lbs</DropdownItem>
-              <DropdownItem onClick={() => setWeightUnit(KG)}>kg</DropdownItem>
-            </DropdownButton>
-          </InputGroup>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <ButtonGroup className='row justify-content-center mb-3 mb-sm-0 button-group-primary'>
-            {ageRadios.map((radio, idx) => (
-              <ToggleButton
-                className='button-standard toggle-button-primary mx-1 col-12 col-sm-3 mb-1 mb-sm-3'
-                key={radio.value}
-                id={`radio-${radio.value}`}
-                type="radio"
-                variant='outline-primary'
-                name="ageRadio"
-                value={radio.value}
-                checked={ageRadioValue === radio.value}
-                onChange={(e) => handleAgeChange(e.currentTarget.value)}
-              >
-                {radio.name}
-              </ToggleButton>
-            ))}
-          </ButtonGroup>
-        </Col>
-      </Row>
+	return (
+		<div>
+			<h1 className='mt-2'>dog</h1>
+			<Row className='mb-4 mt-2 justify-content-center'>
+				<Col className='col-6 col-sm-4'>
+					<InputGroup>
+						<Form.Control id='weightInput' type='number' step='any' placeholder='##' onChange={handleWeightInputChange} />
+						<DropdownButton id='weightDropdown' title={weightUnit}>
+							<DropdownItem onClick={() => setWeightUnit(LBS)}>lbs</DropdownItem>
+							<DropdownItem onClick={() => setWeightUnit(KG)}>kg</DropdownItem>
+						</DropdownButton>
+					</InputGroup>
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<ButtonGroup className='row justify-content-center mb-3 mb-sm-0 button-group-primary'>
+						{ageRadios.map((radio, idx) => (
+							<ToggleButton
+								className='button-standard toggle-button-primary mx-1 col-12 col-sm-3 mb-1 mb-sm-3'
+								key={radio.value}
+								id={`radio-${radio.value}`}
+								type="radio"
+								variant='outline-primary'
+								name="ageRadio"
+								value={radio.value}
+								checked={ageRadioValue === radio.value}
+								onChange={(e) => handleAgeChange(e.currentTarget.value)}
+							>
+								{radio.name}
+							</ToggleButton>
+						))}
+					</ButtonGroup>
+				</Col>
+			</Row>
 
-      {isAdult ?
-        <Row>
-          <Col>
-            <ButtonGroup className='row justify-content-center mb-3 mb-sm-0 button-group-primary'>
-              {neuteredRadios.map((radio, idx) => (
-                <ToggleButton
-                  className='button-standard toggle-button-primary mx-1 col-12 col-sm-3 mb-1 mb-sm-3'
-                  key={radio.value}
-                  id={`radio-${radio.value}`}
-                  type="radio"
-                  variant='outline-primary'
-                  name="neuteredRadio"
-                  value={radio.value}
-                  checked={neuteredRadioValue === radio.value}
-                  onChange={(e) => handleNeuteredChange(e.currentTarget.value)}
-                >
-                  {radio.name}
-                </ToggleButton>
-              ))}
-            </ButtonGroup>
-          </Col>
-        </Row>
-        : null
-      }
+			{isAdult ?
+				<Row>
+					<Col>
+						<ButtonGroup className='row justify-content-center mb-3 mb-sm-0 button-group-primary'>
+							{neuteredRadios.map((radio, idx) => (
+								<ToggleButton
+									className='button-standard toggle-button-primary mx-1 col-12 col-sm-3 mb-1 mb-sm-3'
+									key={radio.value}
+									id={`radio-${radio.value}`}
+									type="radio"
+									variant='outline-primary'
+									name="neuteredRadio"
+									value={radio.value}
+									checked={neuteredRadioValue === radio.value}
+									onChange={(e) => handleNeuteredChange(e.currentTarget.value)}
+								>
+									{radio.name}
+								</ToggleButton>
+							))}
+						</ButtonGroup>
+					</Col>
+				</Row>
+				: null
+			}
 
-      {isAdult && neuteredRadioValue ? (
-        <Row>
-          <Col>
-            <ButtonGroup className='row justify-content-center mb-3 mb-sm-2 button-group-primary'>
-              {activityRadios.map((radio, idx) => (
-                <ToggleButton
-                  className='button-standard toggle-button-primary mx-1 col-12 col-sm-3 mb-1 mb-sm-3'
-                  key={radio.value}
-                  id={`radio-${radio.value}`}
-                  type="radio"
-                  variant='outline-primary'
-                  name="activityRadio"
-                  value={radio.value}
-                  checked={activityRadioValue === radio.value}
-                  onChange={(e) => handleActivityChange(e.currentTarget.value)}
-                >
-                  {radio.name}
-                </ToggleButton>
-              ))}
-            </ButtonGroup>
-          </Col>
-        </Row>
-      ) : null
-      }
-    </div>
-  );
+			{isAdult && neuteredRadioValue ? (
+				<Row>
+					<Col>
+						<ButtonGroup className='row justify-content-center mb-3 mb-sm-2 button-group-primary'>
+							{activityRadios.map((radio, idx) => (
+								<ToggleButton
+									className='button-standard toggle-button-primary mx-1 col-12 col-sm-3 mb-1 mb-sm-3'
+									key={radio.value}
+									id={`radio-${radio.value}`}
+									type="radio"
+									variant='outline-primary'
+									name="activityRadio"
+									value={radio.value}
+									checked={activityRadioValue === radio.value}
+									onChange={(e) => handleActivityChange(e.currentTarget.value)}
+								>
+									{radio.name}
+								</ToggleButton>
+							))}
+						</ButtonGroup>
+					</Col>
+				</Row>
+			) : null
+			}
+		</div>
+	);
 }
 
 export default DogGroup;
--- src/learn.tsx
+++ src/learn.tsx
@@ -1,7 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
 import './learn.scss'
 
-const osuLink = 'https://vet.osu.edu/vmc/companion/our-services/nutrition-support-service/basic-calorie-calculator'
+const osuLink = 'https://vet.osu.edu/vmc/companion/our-services/nutrition-support-service/basic-calorie-calculator';
+const bodyConditionLink = 'https://vet.osu.edu/vmc/companion/our-services/nutrition-support-service/body-condition-muscle-condition-score-charts';
 
 function Learn() {
 	return (
@@ -9,24 +10,55 @@ function Learn() {
 			<Container id='learn-container' className='text-start my-3'>
 				<Row>
 					<Col>
-						{/* <h1 className='learn-page-heading'>Why does it matter?</h1>
-						<p>
-							Your dog's diet is vital to their health and well-being.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-						</p> */}
-
 						<h1 className='learn-page-heading'>How the result is calculated</h1>
-						<p>
+						{/* <p>
 							Dog Calorie Calculator is based on this formula described by the <a href={osuLink} target='_blank' rel='noopener noreferrer'>Ohio State Veterinary Medical Center</a>:
+						</p> */}
+						<p>First, a Resting Energy Requirement (RER) for your dog is calculated using the following formula:</p>
+						<p className='indent'>
+							70(<i>weight in kgs</i>)<sup>3/4</sup> = RER
 						</p>
-						<p className='text-center'>
-							70(weight in kgs)<sup>3/4</sup> * k-factor = result
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-						</p>
+						<p>The RER is then multiplied by a K-factor. The K-factor is determined by your dogs life stage and other factors, such as being spayed/neutered, and activity level.</p>
+
+						<table id='k-factor-table'>
+							<thead>
+								<tr>
+									<th>Some thing</th>
+									<th>k-factor</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Puppy 0-4 months</td>
+									<td>3.0</td>
+								</tr>
+								<tr>
+									<td>Puppy 4-12 months</td>
+									<td>2.0</td>
+								</tr>
+								<tr>
+									<td>Puppy 4-12 months</td>
+									<td>2.0</td>
+								</tr>
+								<tr>
+									<td>Neutered adult</td>
+									<td>1.6</td>
+								</tr>
+								<tr>
+									<td>Intact adult</td>
+									<td>1.8</td>
+								</tr>
+								<tr>
+									<td>Active Adult</td>
+									<td>2.0-5.0</td>
+								</tr>
+								<tr>
+									<td>Inactive Adult</td>
+									<td>1.2-1.4</td>
+								</tr>
+							</tbody>
+						</table>
+						
 					</Col>
 
 				</Row>
