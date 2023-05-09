5972a9a583d90f819e281008015e67371a694a59
Naksp <iw12635@gmail.com>
2023-05-09 15:45:17 -0500

    in progress

--- src/learn.tsx
+++ src/learn.tsx
@@ -1,8 +1,11 @@
 import { Container, Row, Col, Table } from "react-bootstrap";
 import './learn.scss'
 
+// Links
 const osuLink = 'https://vet.osu.edu/vmc/companion/our-services/nutrition-support-service/basic-calorie-calculator';
 const bodyConditionLink = 'https://vet.osu.edu/vmc/companion/our-services/nutrition-support-service/body-condition-muscle-condition-score-charts';
+const canineHealthNutLink = 'https://thecaninehealthnut.com/your-dogs-nutritional-needs/';
+const spotOnTreatsLink = 'https://www.spotondogtraining.org/dog-treats';
 
 function Learn() {
 	return (
@@ -10,13 +13,20 @@ function Learn() {
 			<Container id='learn-container' className='text-start my-3'>
 				<Row>
 					<Col>
-						{/* <h1 className='learn-page-heading'>How the result is calculated</h1> */}
 						<h1 className='learn-page-heading'>How is the result calculated?</h1>
-						<p>First, your dog's Resting Energy Requirement (RER) is calculated. This is the amount of energy needed by your dog in order to perform essential bodily functions, and is based solely on your dog's size. RER is determined by the following equation:</p>
+						<p>
+							First, your dog's Resting Energy Requirement (RER) is calculated.
+							This is the amount of energy needed by your dog in order to perform essential bodily functions, and is based solely on weight.
+							RER is determined by the following equation:
+						</p>
 						<p className='center'>
 							70(<i>weight in kgs</i>)<sup>3/4</sup> = RER
 						</p>
-						<p>The RER is then multiplied by a K-factor. The K-factor is determined by your dogs life stage and other factors, such as being spayed/neutered, and activity level.</p>
+						<p>
+							The RER is then multiplied by a K-factor.
+							The K-factor is determined by your dogs life stage and other factors, such as being spayed/neutered, and activity level.
+							Here's a list of the life factors considered by this calculator, and their corresponding K-factors:
+						</p>
 
 						<div id='table-container'>
 						<Table id='k-factor-table' size='sm' className='center'>
@@ -56,19 +66,43 @@ function Learn() {
 						</div>
 						
 						<p>
-							For example, for a 12kg, 10 month old puppy, the final equation will be:
+							A 12kg, 10 month old puppy, for example, will have a K-factor of 2.0, and the final equation will be:
 						</p>
 						<p className='center'>
 							70(<b>12</b>)<sup>3/4</sup> * <b>2.0</b> = <b>903</b> kcal/day
 						</p>
 
 						<p>
-							Take a look at <a href={osuLink}>Ohio State Veterinary Medical Center's page</a> about this process if you're interested in learning more!
+							Take a look at the <a href={osuLink} target='_blank' rel='noopener noreferrer'>Ohio State Veterinary Medical Center's page</a> about this process if you're interested in learning more!
 						</p>
-						
+					</Col>
+				</Row>
+
+				<Row>
+					<Col>
+						<h1 className='learn-page-heading'>How accurate is the calculation?</h1>
+
+						<p>Not very. .</p>
+					</Col>
+				</Row>
+
+				<Row>
+					<Col>
+						<h1 className='learn-page-heading'>More resources</h1>
+
+						<p>
+							<a href={bodyConditionLink} target='_blank' rel='noopener noreferrer'>Body Condition & Muscle Condition Score Charts</a>
+						</p>
+
+						<p>
+							<a href={canineHealthNutLink} target='_blank' rel='noopener noreferrer'>The Canine Health Nut - Your dog's nutritional needs</a>
+						</p>
+
+						<p>
+							<a href={spotOnTreatsLink} target='_blank' rel='noopener noreferrer'>Spot On Dog Training - Treat recommendations</a>
+						</p>
 
 					</Col>
-
 				</Row>
 
 			</Container>
